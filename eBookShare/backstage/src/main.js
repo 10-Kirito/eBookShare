8dde483f61eb97f946fdbb88ba1bedf88befbe71
selray <124846896+selray@users.noreply.github.com>
2023-04-20 13:50:43 +0800

    更新了后台系统login界面

--- eBookShare/backstage/src/main.js
+++ eBookShare/backstage/src/main.js
@@ -3,13 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/global.css'
+// import request from "@/utls/request";
+import VueParticles from 'vue-particles'
+Vue.use(VueParticles)
+Vue.use(ElementUI, {size: "mini"});
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  store,
+  render: h => h(App)
+}).$mount('#app')

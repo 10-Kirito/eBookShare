39b0760e156cb291baa4a032ceeb3c11614e6457
10-Kirito <2875786463@qq.com>
2023-04-24 15:47:25 +0800

    跨域问题

--- eBookShare/src/main/java/com/example/ebookshare/controller/dto/AdminDTO.java
+++ eBookShare/src/main/java/com/example/ebookshare/controller/dto/AdminDTO.java
@@ -1,10 +1,14 @@
 package com.example.ebookshare.controller.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //接受前端登录请求的参数
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AdminDTO {
     private String username;
     private String password;

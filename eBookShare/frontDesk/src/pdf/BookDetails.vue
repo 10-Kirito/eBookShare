11ca537a95fbf984e28fc4e2d2077f684a5b3e75
10-Kirito <2875786463@qq.com>
2023-05-04 16:43:53 +0800

    修改.gitignore

--- eBookShare/frontDesk/src/pdf/BookDetails.vue
+++ eBookShare/frontDesk/src/pdf/BookDetails.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div>这里是图书详情模板组件!</div>
-
+    <br>
     <el-upload
         class="upload-demo"
         action="http://localhost:9090/file/upload"
@@ -14,10 +14,8 @@
         :file-list="fileList">
       <el-button size="small" type="primary">Click to upload</el-button>
 
-      <div slot="tip" class="el-upload__tip">jpg/png files with a size less than 500kb</div>
+      <div slot="tip" class="el-upload__tip">pdf files</div>
     </el-upload>
-
-    <a href="D:\Desktop\uploadBooks\b8d218cf997144cb9365e0985dbecede.pdf">预览</a>
 
     <br>
     <el-button type="primary" style="margin-top: 10px; margin-bottom: 10px" @click="downLoad">预览</el-button>
@@ -30,7 +28,11 @@ export default {
   name: "BookDetails",
   data(){
     return {
-      fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
+      fileList: [
+        {name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'},
+        {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}
+      ],
+      booksDetail: []
     }
   },
   methods: {
@@ -47,7 +49,7 @@ export default {
       return this.$confirm(`Cancel the transfert of ${ file.name } ?`);
     },
     downLoad(){
-      let url = 'http://localhost:9090/file/4ca86bd15a0d4298a321679f532177bd.pdf';
+      let url = 'http://localhost:9090/file/1de53068902f460b9c7b3bbe99719460.pdf';
       window.open(`/lib/pdfjs-3.5.141-dist/web/viewer.html?file=${url}`);
     }
   }

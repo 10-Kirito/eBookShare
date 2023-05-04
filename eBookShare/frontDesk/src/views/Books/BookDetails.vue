1063cfe3469fcf9d6630508bb1dddc2cf49ddde0
LHC <1941289645@com>
2023-05-04 16:34:46 +0800

    用户客户端：首页至书籍详情页使用路由进行参数的传递，书籍详情页简单设计

--- eBookShare/frontDesk/src/views/Books/BookDetails.vue
+++ eBookShare/frontDesk/src/views/Books/BookDetails.vue
@@ -8,38 +8,119 @@
         5. 下面是关于本书的留言评价，这些数据存放在数据库当中；
         6. 最下面是提供一个评论的入口，用户可以在这里提交自己的评价；
     -->
-    <el-row>
-      <el-col :span="24">
-        <div class="grid-content bg-purple-dark">
+    <div class="parent-container">
+      <el-container class="el-container">
 
-          <el-card class="box-card">
-            <div slot="header" class="clearfix">
-              <span>卡片名称</span>
-              <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+      <el-main>
+        <el-breadcrumb separator-class="el-icon-arrow-right" style="font-size: 20px;margin-bottom: 20px">
+          <el-breadcrumb-item :to="{ path: '/' }" style="color: red;">主页</el-breadcrumb-item>
+          <el-breadcrumb-item>{{ bookDetails.title }}</el-breadcrumb-item>
+        </el-breadcrumb>
+        <el-row class="book-card" :book_id="6138195">
+          <el-col :xs="24" :sm="6" class="book-cover" >
+            <div >
+              <el-image :src="bookDetails.img" fit="cover" lazy style="height: 26pc;"/>
             </div>
-            <div v-for="o in 4" :key="o" class="text item">
-              {{'列表内容 ' + o }}
-            </div>
-          </el-card>
-        </div>
-      </el-col>
-    </el-row>
+
+          </el-col>
+          <el-col :xs="24" :sm="18" class="book-info">
+            <el-row class="align-left">
+              <h1 class="book-title" style="margin: 0;padding: 0">{{ bookDetails.title }}</h1>
+            </el-row>
+            <el-row class="align-left">
+              <i class="book-author">
+                <a href="/author/J. D. Salinger" title="找到该作者的所有书籍" class="color1">{{ bookDetails.author }}</a>
+              </i>
+            </el-row>
+            <el-row class="align-left">
+                <div style="font-size: 20px;display: inline-block">
+                  <el-tooltip class="item" effect="dark" content="收藏" placement="bottom">
+                    <i :class="collectBtnClass" @click="collectBtn" style="cursor: pointer;font-size: 25px;margin-right: 5px"></i>
+                  </el-tooltip>
+                  <el-tooltip class="item" effect="dark" content="图书评级/文件质量" placement="bottom">
+                    <i>
+                      <span class="book-rating-interest-score">5.0</span> /
+                      <span class="book-rating-quality-score">4.0</span>
+                    </i>
+                  </el-tooltip>
+                </div>
+
+              <div style="display: inline-block" class="ml-5">
+                <i class="el-icon-chat-dot-square" style="cursor: pointer;font-size: 25px;margin-right: 5px">
+                  <span style="font-size: 20px;margin-right: 5px">1 comment</span>
+                </i>
+              </div>
+
+              <el-tooltip content="加入书单">
+                <el-button class="book-add-to-list" icon="el-icon-folder-add" circle />
+              </el-tooltip>
+              <el-tooltip content="感谢贡献者">
+                <el-button class="book-thank-contributors" icon="el-icon-thumb" circle />
+              </el-tooltip>
+            </el-row>
+
+            <el-row justify="space-between">
+              <p class="grid-content" style="text-align: justify">{{bookDetails.description}}</p>
+            </el-row>
 
 
-
-
-
-    <el-backtop target=".page-component__scroll .el-scrollbar__wrap"></el-backtop>
+            <el-row :gutter="20"  class="row-bg" >
+              <el-col :span="2" ><div class="grid-content ">种类:</div></el-col>
+              <el-col :span="6" ><div class="grid-content ">{{bookDetails.category}}</div></el-col>
+              <el-col :span="2" ><div class="grid-content ">年：</div></el-col>
+              <el-col :span="6" ><div class="grid-content ">{{bookDetails.publishedDate}}</div></el-col>
+            </el-row>
+            <el-row :gutter="20"  class="row-bg" >
+              <el-col :span="2"><div class="grid-content ">出版社:</div></el-col>
+              <el-col :span="6"><div class="grid-content ">{{bookDetails.publisher}}</div></el-col>
+              <el-col :span="2"><div class="grid-content ">语言：</div></el-col>
+              <el-col :span="6"><div class="grid-content ">{{bookDetails.Language}}</div></el-col>
+            </el-row>
+            <el-row :gutter="20"  class="row-bg" >
+              <el-col :span="2"><div class="grid-content ">ISBN:</div></el-col>
+              <el-col :xs="8" :sm="6" :md="40" :lg="50" :xl="10"><div class="grid-content ">{{bookDetails.ISBN}}</div></el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+        <el-row type="flex" justify="left" >
+          <el-button type="primary" icon="el-icon-download" plain style="margin-left: 30px;width: 300px">下载</el-button>
+        </el-row>
+      </el-main>
+    </el-container>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "BookDetails",
+  created() {
+    const params = this.$route.query.params;
+    if (params) {
+      try {
+        this.bookDetails = JSON.parse(params);
+        console.log('数据是：', this.bookDetails);
+      } catch (e) {
+        console.error('解析参数出错：', e);
+      }
+    } else {
+      console.warn('未找到参数');
+    }
+  },
   data() {
     return {
-      bookDetails: {
-        img: 'https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg'
+      bookDetails: {},
+      collectBtnClass:"el-icon-star-off",
+      isCollect:false
+    }
+  },
+  methods:{
+    collectBtn(){//收藏
+      this.isCollect=!this.isCollect
+      if(this.isCollect){
+        this.collectBtnClass="el-icon-star-off"
+      }else{  //取消收藏
+        this.collectBtnClass="el-icon-star-on"
       }
     }
   }
@@ -47,24 +128,80 @@ export default {
 </script>
 
 <style scoped>
-.text {
-  font-size: 14px;
+.parent-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh; /* 设置高度以垂直居中容器 */
 }
 
-.item {
-  margin-bottom: 18px;
+.el-container {
+  transform: scale(0.8); /* 缩小容器 */
 }
 
-.clearfix:before,
-.clearfix:after {
-  display: table;
-  content: "";
-}
-.clearfix:after {
-  clear: both
+.book-title{
+  font-size: 40px;
 }
 
-.box-card {
-  width: 480px;
+.align-left {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+
+.ml-5{
+  margin-left: 25px;
+}
+
+.book-rating-detail .book-comments-info {
+  display: inline-block;
+  padding-left: 24px;
+  cursor: pointer;
+}
+
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  text-align: left;
+  margin-left: 0px;
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 0px 0px;
+  background-color: #f9fafc;
+}
+
+.color1 {
+  font-size: 20px;
+  color: #49afd0;
+}
+.book-rating-interest-score{
+  color: #FAAD14;
+}
+.book-rating-quality-score{
+  color: #5CB85C;
+}
+div {
+  margin: 0;
+  padding: 0;
 }
 </style>
--- eBookShare/frontDesk/src/views/Layout.vue
+++ eBookShare/frontDesk/src/views/Layout.vue
@@ -14,20 +14,20 @@
       <!--  首页主要组成部分-->
       <el-main class="el-main">
         <div style="margin-bottom: 10px; margin-top: 20px">
-          <input type="text" v-model="searchQuery" placeholder="输入关键字搜索图书">
-          <button @click="searchBooks" style="height: 64px;width: 100px; font-size: 20px">查询</button>
-
+          <el-row style="margin-bottom:30px">
+            <input type="text" v-model="searchQuery" placeholder="输入关键字搜索图书">
+            <button @click="searchBooks" style="height: 64px;width: 100px; font-size: 20px">查询</button>
+          </el-row>
           <!-- 走马灯设置-->
-          <div style="margin-top: 20px">
-            <el-carousel :interval="4000" type="card" height="400px">
-              <el-carousel-item v-for="item in 6" :key="item">
-                <h3 class="medium">{{ item }}</h3>
-              </el-carousel-item>
-            </el-carousel>
-          </div>
+<!--          <div style="margin-top: 20px">-->
+<!--            <el-carousel :interval="4000" type="card" height="400px">-->
+<!--              <el-carousel-item v-for="item in 6" :key="item">-->
+<!--                <h3 class="medium">{{ item }}</h3>-->
+<!--              </el-carousel-item>-->
+<!--            </el-carousel>-->
+<!--          </div>-->
 
           <el-row>
-
             <!-- 左侧显示内容-->
             <el-col :span="6" style="color: #42b983">
               <div class="grid-content bg-purple" >
@@ -45,10 +45,17 @@
             <el-col :span="12">
               <div class="grid-content bg-purple-light">
                 <h1>今日推荐阅读图书：</h1>
+
                 <el-card class="box-card">
-                  <div v-for="o in 4" :key="o" style="font-size: 14px; padding: 18px 0; width: 480px;">
-                    {{'列表内容 ' + o }}
-                  </div>
+                  <el-row :gutter="20">
+                    <el-col :span="6" class="book-cover" >
+                        <el-image :src="bookDetails[0].img" fit="contain" lazy style="height: 150px;"/>
+                    </el-col>
+                    <el-col :span="10" >
+                      <el-row class="attriclass" ><h3 @click="pushDetail" style="cursor: pointer">{{ bookDetails[0].title }}</h3></el-row>
+                      <el-row class="attriclass"><div class="grid-content ">{{bookDetails[0].publisher}}</div></el-row>
+                    </el-col>
+                  </el-row>
                 </el-card>
 
                 <el-card class="box-card" style="margin-top: 10px">
@@ -98,6 +105,7 @@
                 </div>
               </div>
             </el-col>
+
           </el-row>
 
         </div>
@@ -110,29 +118,73 @@
         </div>
       </el-footer>
     </el-container>
-
   </div>
 </template>
 
 
 
 <script>
+import bookDetails from "@/views/Books/BookDetails.vue";
+import router from "@/router/index.js"
+
+
 export default {
   name: "Layout",
   data() {
     return {
-      pageTitle: '上海大学电子图书分享平台'
+      pageTitle: '上海大学电子图书分享平台',
+      bookDetails: [{
+        img: "https://bookcover.yuewen.com/qdbimg/349573/1019103033/180",
+        title: "Book Title",
+        author: "Author Name",
+        publisher: "Publisher Name",
+        publishedDate: "2022-01-01",
+        description: "《麦田里的守望者》的主人公,16岁的中学生霍尔顿·考尔菲德是当代美国文学中最早出现的反英雄形象之一。霍尔顿出身在纽约一个富裕的中产阶级的家庭。学校里的老师和自己的家长强迫他好好读书,为的是“出人头地,而他看不惯周围的一切,根本没心思用功读书,因而老是挨罚。他的内心又十分苦闷、彷徨,这种精神上无法调和的极度矛盾最终令他彻底崩溃,躺倒在精神病院里。",
+        ISBN: "978-3-16-148410-0",
+        Format: "Hardcover",
+        Pages: "400",
+        Language: "English",
+        category:"category"
+      }],
+      searchQuery:''
     };
   },
   methods: {
     searchBooks() {
       // 这里可以向服务器发起搜索请求，获取匹配的图书列表
     },
+    pushDetail(){
+      // console.log(JSON.stringify(this.bookDetails[0]))
+      router.push({
+        path:'/bookdetails',
+        query:{params:JSON.stringify(this.bookDetails[0])}
+      })
+    }
   },
 }
 </script>
 
 <style scoped>
+
+.attriclass{
+  padding: 0;
+  margin: 0;
+}
+
+.align-left {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.grid-content {
+  text-align: left;
+  margin-left: 0px;
+  border-radius: 4px;
+  min-height: 36px;
+}
+
 .el-header{
   background-color: #B3C0D1;
   color: #333;
